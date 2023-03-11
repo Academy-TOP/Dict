4396118bf7e6be5bb5a6d2e55e39b483961fa7cd
Евгений Тищук <36456429+Jack36VRN@users.noreply.github.com>
2023-03-11 11:08:43 +0000

    1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-my_dict = {}
+my_dict = {"Developer": "Ann"}
 my_dict_2 = {"Macbook": "Pro", "Developer": "Nick", "Iphone": 1}
 
 value = my_dict_2["Macbook"]
@@ -8,6 +8,8 @@ value = "orange"
 
 my_dict_2[key] = value
 my_dict_2['Картина'] = 'Мишки в лесу'
+
+my_dict.update(my_dict_2)
 
 print(my_dict)
 print(my_dict_2)
