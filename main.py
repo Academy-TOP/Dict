b230f64fbd1aed84a4eb7896bad4eeba647343e7
Евгений Тищук <36456429+Jack36VRN@users.noreply.github.com>
2023-03-11 11:02:53 +0000

    1

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ my_dict_2 = {"Macbook": "Pro", "Developer": "Nick", "Iphone": 1}
 
 value = my_dict_2["Macbook"]
 
+key = "fruit"
+value = "orange"
+
+my_dict_2[key] = value
+
 print(my_dict)
 print(my_dict_2)
 print(value)
