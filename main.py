a732bcef204298366e782538cace7598b4607e00
Евгений Тищук <36456429+Jack36VRN@users.noreply.github.com>
2023-03-11 11:15:55 +0000

    1

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ my_dict_2['Картина'] = 'Мишки в лесу'
 
 my_dict.update(my_dict_2)
 
+my_dict_3 = dict.fromkeys(("Macbook", "Iphone"), 1)
+
 print(my_dict)
 print(my_dict_2)
 print(value)
+
+print(my_dict_3)
