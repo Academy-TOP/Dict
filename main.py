e25a9ae27f69c7862061a87418a371e9afa3bf67
Евгений Тищук <36456429+Jack36VRN@users.noreply.github.com>
2023-03-11 11:01:15 +0000

    1

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 my_dict = {}
-my_dict_2 = {"Macbook" : "Pro", "Developer" : "Nick", "Iphone": 1}
+my_dict_2 = {"Macbook": "Pro", "Developer": "Nick", "Iphone": 1}
 
 value = my_dict_2["Macbook"]
 
 print(my_dict)
 print(my_dict_2)
+print(value)
